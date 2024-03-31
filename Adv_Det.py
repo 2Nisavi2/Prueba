b2387b3d5e737223e789abf9c20e0cbfd1febd1b
Nicolás Ávila <avila220895@gmail.com>
2024-03-31 14:04:26 -0500

    Load model det

--- Adv_Det.py
+++ Adv_Det.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 
 ### --Load Functions-- ##
 from Prueba.functions.load_DS import load_ds
+from Prueba.functions.load_model import load_model
 
 ### --LOAD DATASET-- ###
 ## Load the dataset to use in the neural network models. Posible datasets:
@@ -41,4 +42,31 @@ from Prueba.functions.load_DS import load_ds
 # 7   Class Sneaker
 # 8   Class Bag
 # 9   Class Ankle boot
-data = load_ds('mnist')
+data = load_ds('mnist',5)
+
+## This code allow the load of the model follow the kind of neural network. This is the list of possible models to load:
+# 'vgg16_mnist'             Deterministic vgg16 model in mnist dataset
+# 'vgg19_mnist'             Deterministic vgg19 model in mnist dataset
+# 'vgg16bt_mnist'           Full MNF Bayesian vgg16 model in mnist dataset
+# 'vgg19bt_mnist'           Full MNF Bayesian vgg19 model in mnist dataset
+# 'vgg16b1_mnist'           Last layer MNF Bayesian vgg16 model in mnist dataset
+# 'vgg19b1_mnist'           Last layer MNF Bayesian vgg19 model in mnist dataset
+# 'vgg16bt_Re_mnist'        Full Reparameterization Trick Bayesian vgg16 model in mnist dataset
+# 'vgg19bt_Re_mnist'        Full Reparameterization Trick Bayesian vgg19 model in mnist dataset
+# 'vgg16b1_Re_mnist'        Last layer Reparameterization Trick Bayesian vgg16 model in mnist dataset
+# 'vgg19b1_Re_mnist'        Last layer Reparameterization Trick Bayesian vgg19 model in mnist dataset
+# 'vgg16_fashion'           Deterministic vgg16 model in fashion_mnist dataset
+# 'vgg19_fashion'           Deterministic vgg19 model in fashion_mnist dataset
+# 'vgg16bt_fashion'         Full MNF Bayesian vgg16 model in fashion_mnist dataset
+# 'vgg19bt_fashion'         Full MNF Bayesian vgg19 model in fashion_mnist dataset
+# 'vgg16b1_fashion'         Last layer MNF Bayesian vgg16 model in fashion_mnist dataset
+# 'vgg19b1_fashion'         Last layer MNF Bayesian vgg19 model in fashion_mnist dataset
+# 'vgg16bt_Re_fashion'      Full Reparameterization Trick Bayesian vgg16 model in fashion_mnist dataset
+# 'vgg19bt_Re_fashion'      Full Reparameterization Trick Bayesian vgg19 model in fashion_mnist dataset
+# 'vgg16b1_Re_fashion'      Last layer Reparameterization Trick Bayesian vgg16 model in fashion_mnist dataset
+# 'vgg19b1_Re_fashion'      Last layer Reparameterization Trick Bayesian vgg19 model in fashion_mnist dataset
+# 'vgg16bt_CA'              Full Cauchy MNF Bayesian vgg16 model in mnist dataset
+# 'vgg16b1_CA'              Full Cauchy MNF Bayesian vgg16 model in mnist dataset
+# 'vgg16bt_GUM'             Full Gumbel MNF Bayesian vgg16 model in mnist dataset
+# 'vgg16b1_GUM'             Full Cumbel MNF Bayesian vgg16 model in mnist dataset
+Model = load_model('vgg16_mnist')

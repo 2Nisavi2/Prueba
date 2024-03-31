72548c3782b73f1a03079fcbcbf17d4f6b1584a0
Nicolás Ávila <avila220895@gmail.com>
2024-03-31 13:54:31 -0500

    Categories on DS function

--- Adv_Det.py
+++ Adv_Det.py
@@ -1,5 +1,4 @@
-##Load Libraries
-import tensorflow as tf
+## --Load Libraries-- ##
 import argparse
 import pathlib
 from tensorflow.keras import layers
@@ -11,6 +10,35 @@ from sklearn.decomposition import PCA
 import os
 import matplotlib.pyplot as plt
 
-## Load Functions
+### --Load Functions-- ##
 from Prueba.functions.load_DS import load_ds
+
+### --LOAD DATASET-- ###
+## Load the dataset to use in the neural network models. Posible datasets:
+# 'mnist'             Available in all the models
+# 'fashion_mnist'     Not available in Cauchy and Gumbal vgg16 models
+
+## For the categories select a number between 0 and 9. Follow the dataset, the numbers rep´resent a especific category:
+## MNIST Categories:
+# 0   Class 0
+# 1   Class 1
+# 2   Class 2
+# 3   Class 3
+# 4   Class 4
+# 5   Class 5
+# 6   Class 6
+# 7   Class 7
+# 8   Class 8
+# 9   Class 9
+## FASHION-MNIST Categories:
+# 0   Class T-shirt/top
+# 1   Class Trouser
+# 2   Class Pullover
+# 3   Class Dress
+# 4   Class Coat
+# 5   Class Sandal
+# 6   Class Shirt
+# 7   Class Sneaker
+# 8   Class Bag
+# 9   Class Ankle boot
 data = load_ds('mnist')
--- functions/load_DS.py
+++ functions/load_DS.py
@@ -1,37 +1,39 @@
-## LOAD DATASET
-
+### --LOAD DATASET-- ###
 ## Load the dataset to use in the neural network models. Posible datasets:
-## mnist             available in all the models
-## fashion_mnist     Not available in Cauchy and Gumbal vgg16 models
+# 'mnist'             available in all the models
+# 'fashion_mnist'     Not available in Cauchy and Gumbal vgg16 models
 
 ## For the categories select a number between 0 and 9. Follow the dataset, the numbers rep´resent a especific category:
 ## MNIST Categories:
-## 0   Class 0
-## 1   Class 1
-## 2   Class 2
-## 3   Class 3
-## 4   Class 4
-## 5   Class 5
-## 6   Class 6
-## 7   Class 7
-## 8   Class 8
-## 9   Class 9
+# 0   Class 0
+# 1   Class 1
+# 2   Class 2
+# 3   Class 3
+# 4   Class 4
+# 5   Class 5
+# 6   Class 6
+# 7   Class 7
+# 8   Class 8
+# 9   Class 9
 ## FASHION-MNIST Categories:
-## 0   Class T-shirt/top
-## 1   Class Trouser
-## 2   Class Pullover
-## 3   Class Dress
-## 4   Class Coat
-## 5   Class Sandal
-## 6   Class Shirt
-## 7   Class Sneaker
-## 8   Class Bag
-## 9   Class Ankle boot
+# 0   Class T-shirt/top
+# 1   Class Trouser
+# 2   Class Pullover
+# 3   Class Dress
+# 4   Class Coat
+# 5   Class Sandal
+# 6   Class Shirt
+# 7   Class Sneaker
+# 8   Class Bag
+# 9   Class Ankle boot
 
 def load_ds (DS, Category):
 
-    ## Load Dataset
+    ##Load Libraries
+    import tensorflow as tf
     import tensorflow_datasets as tfds
+
+    ## Load Dataset
     (train_ds,test_ds_train,test_ds_test,test_ds_testadv), INFO = tfds.load(name = DS,
                                     split = ['train[:90%]','test[:90%]','test[90%:99%]','test[99%:]'],
                                     as_supervised = True,
